6081e03a3aac70f45f8d3cc21c771d50cc91a746
sean stephen brian <seanstephenbrian@gmail.com>
2022-08-11 15:38:54 -0400

    Use Javascript to add dynamic grid to sketch field
    Take user input (for now just gathering this with a prompt) for any
    number x and use a loop to create a grid of size x by x within the
    sketch field container

--- etch.js
+++ etch.js
@@ -1,1 +1,14 @@
- 
+const sketchField = document.querySelector('.sketch-field');
+
+let x = prompt('how many cells wide','');
+
+for (i=1; i<=x*x; i++) {
+
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    sketchField.setAttribute('style', `grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr);`)
+
+    sketchField.appendChild(cell);
+
+ }
