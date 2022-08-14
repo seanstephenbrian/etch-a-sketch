e172803cd07ae39a33695ab0ea361a0e5c187823
seanstephenbrian <seanstephenbrian@gmail.com>
2022-08-13 22:17:33 -0400

    Begin work on random image selector
    Set up architecture for new function 'pickArt' to randomly select one of
    ten art images; need to download/edit image files and write captions

--- etch.js
+++ etch.js
@@ -3,6 +3,7 @@ const colorPicker = document.querySelector('#color-picker');
 const pixelPicker = document.querySelector('#pixel-picker');
 const pixelType = document.querySelector('.pixel-type');
 const artTitle = document.querySelector('.art-title');
+const artImg = document.querySelector('.art-img');
 
 let pixels = pixelPicker.getAttribute('value');
 
@@ -75,6 +76,43 @@ function changePixelPicker(event) {
     }
     addListeners();
 }
+
+function pickArt() {
+    let x = Math.floor(Math.random() * 11);
+    if (x === 1) {
+        artTitle.innerHTML = 'Georges Seurat, <span style="font-style: italic;">A Sunday Afternoon on the Island of La Grande Jatte</span>, 1884-1886, oil on canvas.';
+        artImg.setAttribute('src','img/seurat.jpg');
+    } else if (x === 2) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 3) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 4) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 5) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 6) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 7) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 8) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 9) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    } else if (x === 10) {
+        artTitle.innerHTML = '';
+        artImg.setAttribute('src','');
+    }
+}
+
+pickArt();
 
 for (i=1; i<=pixels*pixels; i++) {
     createGrid(pixels);
