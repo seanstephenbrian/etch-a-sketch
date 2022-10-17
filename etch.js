ed2af439bdc210b2fd0e6452210a80627f8b3a89
seanstephenbrian <seanstephenbrian@gmail.com>
2022-10-17 14:29:57 -0400

    Clean up code slightly, add comments throughout

--- etch.js
+++ etch.js
@@ -1,3 +1,4 @@
+// set up DOM variables:
 const sketchField = document.querySelector('.sketch-field');
 const colorPicker = document.querySelector('#color-picker');
 const pixelPicker = document.querySelector('#pixel-picker');
@@ -6,30 +7,48 @@ const artTitle = document.querySelector('.art-title');
 const artImg = document.querySelector('.art-img');
 const newMasterpiece = document.querySelector('.new-masterpiece');
 
+// add listener to trigger changeColorPicker function when colorPicker input value changes:
+colorPicker.addEventListener('input', changeColorPicker);
+
+// add listener to trigger changePixelPicker function when pixelPicker input value changes:
+pixelPicker.addEventListener('input', changePixelPicker);
+
+// store initial value of pixel picker in 'pixels' variable:
 let pixels = pixelPicker.getAttribute('value');
 
+// store initial value of color picker in 'color' variable:
 let color = colorPicker.getAttribute('value');
 
+// trigger pickArt function when user clicks newMasterpiece button:
 newMasterpiece.addEventListener('click', () => {
-    pickArt();
+    changeArt();
 });
 
-function createGrid(x) {
+// create a single cell div within the sketch grid:
+function createGridCell(x) {
     const cell = document.createElement('div');
 
     cell.classList.add('cell');
 
     cell.classList.add('rounded-cell');
 
-    sketchField.setAttribute('style', `grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr);`);
-
     sketchField.appendChild(cell);
-
-    colorPicker.addEventListener('input', changeColorPicker);
-
-    pixelPicker.addEventListener('input', changePixelPicker);
 }
 
+// render the sketch field by creating the selected number of cell divs and styling them in a square css grid:
+function renderGrid(pixels) {
+    for (i=1; i<=pixels*pixels; i++) {
+        createGridCell();
+    }
+    sketchField.setAttribute('style', `grid-template-columns: repeat(${pixels}, 1fr); grid-template-rows: repeat(${pixels}, 1fr);`);
+    
+    addListeners();
+}
+
+// render the initial sketch grid:
+renderGrid(pixels);
+
+// add the listeners necessary for decent click/drag behavior in the sketch field:
 function addListeners() {
 
     let mouseDown = false;
@@ -65,26 +84,26 @@ function addListeners() {
             cell.setAttribute('style', `background-color: ${color};`);
         });
     });
-
 };
 
+// set the 'color' variable to the current colorPicker input value:
 function changeColorPicker(event) {
     color = event.target.value;
 }
 
+// set the 'pixels' variable to the current pixelPicker input value and re-render the grid whenever the pixel selection changes:
 function changePixelPicker(event) {
     pixels = event.target.value;
     i = 1;
     sketchField.innerHTML = '';
-    for (i=1; i<=pixels*pixels; i++) {
-        createGrid(pixels);
-    }
-    addListeners();
+    renderGrid(pixels);
 }
 
+// set counter for the art display to its initial value:
 let x = 0;
 
-function pickArt() {
+// change the art, advancing through a selection of 10 images:
+function changeArt() {
     x += 1;
     if (x === 1) {
         artTitle.innerHTML = 'Georges Seurat, <span style="font-style: italic;">A Sunday Afternoon on the Island of La Grande Jatte</span>, 1884-86, oil on canvas.';
@@ -120,14 +139,10 @@ function pickArt() {
     }
 }
 
-pickArt();
+// set the initial art image:
+changeArt();
 
-for (i=1; i<=pixels*pixels; i++) {
-    createGrid(pixels);
-}
-
-addListeners();
-
+// toggle between rounded and square cell styling for the sketch grid:
 function changePixelType() {
     const cells = document.querySelectorAll('.cell');
     cells.forEach(cell => {
@@ -135,6 +150,7 @@ function changePixelType() {
     });
 }
 
+// add click listener on the pixelType button to trigger the changePixelType function:
 pixelType.addEventListener('click', () => {
     changePixelType();
 });
